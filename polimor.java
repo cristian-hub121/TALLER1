a4bf7b3bb3697638104384dac3ec03a83c78f5fa
cristian-hub121 <cristiansamuelcor@gmail.com>
2025-10-30 10:33:05 -0500

    Create polimor.java

---
+++ polimor.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pood2.polimorfismo;
+
+/**
+ *
+ * @author ALUMNO#
+ */
+public class Polimorfismo {
+    //primera
+
+    static int operacion(int n1, int n2){
+
+        return(n1+n2);
+
+
+
+
+        //segunda
+
+    }
+        static double operacion(double n1, double n2){
+
+            return(n1*n2);
+
+        //tercera
+        }
+
+            static int operacion(int n1, int n2,int n3){
+
+        return(n1+n2+n3);
+
+            }
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("resultado:"+ operacion(2,4));
+    }
+}
