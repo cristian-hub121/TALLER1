24dcd9e0a93ecf5fe62faf9d8dbbf5dc67b950ee
cristian-hub121 <cristiansamuelcor@gmail.com>
2025-10-30 10:35:00 -0500

    Create taller(pasado).java

---
+++ taller(pasado).java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pood2.taller1;
+
+/**
+ *
+ * @author ALUMNO#
+ */
+public class Taller1 {
+
+    public static void main(String[] args) {
+        int num1 = 6, num2 = 8,suma;
+           suma = num1 + num2;
+        int num3 = 5, num4 = 6,resta;
+                resta = num3-num4;
+                int num5 = 13, num7 = 70,multi;
+                multi = num5 * num7;
+        System.out.println("multi ="+multi);
+        System.out.println("suma ="+suma);
+        System.out.println("resta ="+resta);
+
+    }
+}
