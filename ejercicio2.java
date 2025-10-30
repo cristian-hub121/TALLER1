c766a60558927ebca42123c7c48100b313cf3823
cristian-hub121 <cristiansamuelcor@gmail.com>
2025-10-30 10:36:18 -0500

    Create ejercicio2.java

---
+++ ejercicio2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pood2.taller1;
+
+/**
+ *
+ * @author ALUMNO#
+ */
+public class ejercicio2 {
+    public static void main(String[] args) {
+   int kwhconsumi = 200;
+   int costoporkwh = 400;
+        double subtotal = kwhconsumi * costoporkwh;
+            double iva = 0.16;
+          double  totalporiva = iva * subtotal;
+       System.out.println("multi ="+totalporiva);
+
+
+    }
