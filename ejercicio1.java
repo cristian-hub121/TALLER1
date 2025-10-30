fa6bdd5cd90f68e1e0a0f14a59c21b18ac14b733
cristian-hub121 <cristiansamuelcor@gmail.com>
2025-10-30 10:35:41 -0500

    Create ejercicio1.java

---
+++ ejercicio1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pood2.taller1;
+
+/**
+ *
+ * @author ALUMNO#
+ */
+public class ejercicio1 {
+      public static void main(String[] args) {
+    int precio= 50000;
+          double descuento = 0.20;
+             double montodescontado = precio * descuento;
+                     double preciofinal = precio - montodescontado;
+    System.out.println("el precio final es de:"+preciofinal);
+    
+}  
+ }
